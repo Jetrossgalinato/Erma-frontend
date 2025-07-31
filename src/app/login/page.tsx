08c95e913409cafe1b3973224576d9d50a3c1957
Jetross <jetrossneri07@gmail.com>
2025-07-31 13:27:13 +0800

    added initial code for login page and register page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="text-center text-2xl font-bold">
+      <h1>This is the login page</h1>
+    </div>
+  );
+}
---
+++ src/app/register/page.tsx
@@ -0,0 +1,7 @@
+export default function RegisterPage() {
+  return (
+    <div className="text-center text-2xl font-bold">
+      <h1>This is the register page</h1>
+    </div>
+  );
+}
