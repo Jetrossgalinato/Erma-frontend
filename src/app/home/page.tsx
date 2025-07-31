7f9b01f338a1ed783eec37d804d8dd7aef283517
Jetross <jetrossneri07@gmail.com>
2025-07-31 13:24:26 +0800

    installed lucide-react

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="text-center">
+    <div className="text-center text-2xl font-bold">
       <h1>This is the home page</h1>
     </div>
   );
