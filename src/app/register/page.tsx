73f62bcc78b1164f888c15c68ade4636e82bdffe
Jetross <jetrossneri07@gmail.com>
2025-07-31 13:42:32 +0800

    added navbar on register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+import Navbar from "../components/Navbar";
+
 export default function RegisterPage() {
   return (
-    <div className="text-center text-2xl font-bold">
-      <h1>This is the register page</h1>
+    <div className="min-h-screen bg-gray-50">
+      <Navbar />
+      <div className="text-center text-black text-2xl font-bold mt-10">
+        <h1>This is the Register page</h1>
+      </div>
     </div>
   );
 }
