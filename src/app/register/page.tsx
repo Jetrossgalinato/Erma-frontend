4aae5f05eefe29b50635a9412f2fc6f880f88786
Jetross <jetrossneri07@gmail.com>
2025-08-01 14:11:18 +0800

    finally fixed the avatar icon issue on navbar

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { supabase } from "../../../supabaseClient";
+import { supabase } from "../../../lib/supabaseClient";
 import Navbar from "../../components/Navbar";
 
 export default function RegisterPage() {
