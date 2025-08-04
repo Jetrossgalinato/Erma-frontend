6a94737e9a1d4b9b94f46541553b72a9421cd4cc
Jetross <jetrossneri07@gmail.com>
2025-08-04 08:26:25 +0800

    made a request page

--- src/app/dashboardRoles/superAdmin/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function SuperDashboardPage() {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <div className="flex-1 flex items-center justify-center">
-        <h1 className="text-3xl font-bold">Super Admin Dashboard</h1>
-      </div>
-    </div>
-  );
-}
---
+++ src/app/requests/page.tsx
@@ -0,0 +1,8 @@
+export default function RequestsPage() {
+  return (
+    <div>
+      <h1>Requests</h1>
+      <p>Here you can manage your requests.</p>
+    </div>
+  );
+}
