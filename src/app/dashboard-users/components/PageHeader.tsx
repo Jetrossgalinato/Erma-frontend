96f3cb44e06de010c785c002a61e0c12833b5f9c
Jetross <jetrossneri07@gmail.com>
2026-02-02 19:39:28 +0800

    fix: grammaticaly incorrect titles

--- src/app/dashboard-users/components/PageHeader.tsx
+++ src/app/dashboard-users/components/PageHeader.tsx
@@ -13,7 +13,7 @@ export default function PageHeader({
     <div className="flex flex-col gap-4">
       <div>
         <h1 className="text-3xl font-semibold text-gray-900 dark:text-gray-100 tracking-tight">
-          Users Management
+          User Management
         </h1>
         <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
           Manage and view all account requests from users in the system
